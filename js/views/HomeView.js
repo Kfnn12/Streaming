e5b224f3ecd5525a038cbab267d81245b2b3dd9c
Kfnn12 <132144405+Kfnn12@users.noreply.github.com>
2026-04-09 14:23:29 +0800

    Create HomeView.js

---
+++ js/views/HomeView.js
@@ -0,0 +1,7 @@
+import { createAnimeCard } from '../utils.js';
+import { renderHero } from '../components/Hero.js';
+
+export async function renderHome(container, homeData, showDetails, showCategory) {
+  // All your previous renderHome logic using components
+  // Much cleaner and reusable
+}
