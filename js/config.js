0346e0b34eac0e3d74f75b81138c2ff952adc311
Kfnn12 <132144405+Kfnn12@users.noreply.github.com>
2026-04-09 14:13:25 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+export const BASE_API_URL = 'https://ogkgn.vercel.app';
+
+export const M3U8_PROXIES = [
+  'https://proxy1-delta-lake.vercel.app/m3u8-proxy?url=',
+  'https://hianimeproxy-olive.vercel.app/m3u8?url=',
+  'https://animepahe-proxy.vercel.app/m3u8-proxy?url='
+];
